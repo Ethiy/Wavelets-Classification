e16a8e5a15a353fdf0e57c70f739dc0db2b26439
Ethiy <oussama.ennafii@polytechnique.org>
2015-11-09 21:14:51 +0100

    initial commit

---
+++ project/auxilary/create_dreemsrc.m
@@ -0,0 +1,31 @@
+
+function src = create_dreemsrc(labels)
+
+
+files = [];
+classes = {'pos', 'neg'};
+
+%  objects = [];
+
+
+parfor ind = 1: length(labels) %   files)
+    
+disp(['runing parfor ...i = ', int2str(ind)]);    
+    new_object{ind}.ind = ind;
+    new_object{ind}.u1 = 1;
+    new_object{ind}.u2 = 600;
+    new_object{ind}.class = (labels(ind)==0)*2 + (labels(ind) == 1 );
+%     objects(ind) = new_object{ind} ;
+end
+
+disp('done parfor');
+
+objects  = new_object{1};
+parfor i = 2 : length(labels)
+    objects(i) = new_object{i};
+end
+
+src.classes = classes;
+src.files = files;
+src.objects = objects;
+end
